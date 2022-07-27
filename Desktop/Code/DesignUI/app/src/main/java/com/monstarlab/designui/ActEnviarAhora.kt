ad6efc3d63403e6f6cb26f86a27a8e9513cf3e48
binh17021995 <binh.td95@gmail.com>
2022-07-27 15:48:00 +0700

    first commit

---
+++ Desktop/Code/DesignUI/app/src/main/java/com/monstarlab/designui/ActEnviarAhora.kt
@@ -0,0 +1,11 @@
+package com.monstarlab.designui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActEnviarAhora : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_enviar_ahora)
+    }
+}
---
+++ Desktop/Code/DesignUI/app/src/main/java/com/monstarlab/designui/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.monstarlab.designui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_layout)
+    }
+}
